98f36eb6af4e5fa8d692e6a5382356a9857feef0
Issa-barry <issabarry67@gmail.com>
2025-09-19 17:15:19 +0200

    seeder

---
+++ database/seeders/FraisSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Frais;
+
+class FraisSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Barème par défaut : 5% applicable à partir de 0€ (sans plafond)
+        $rows = [
+            [
+                'nom'         => 'standard',
+                'type'        => 'pourcentage', // 'fixe' ou 'pourcentage'
+                'valeur'      => 5,             // 5 => 5%
+                'montant_min' => 0,
+                'montant_max' => 100,          // null = sans limite supérieure
+            ],
+        ];
+
+        foreach ($rows as $data) {
+            Frais::updateOrCreate(
+                ['nom' => $data['nom'], 'type' => $data['type']],
+                $data
+            );
+        }
+    }
+}
